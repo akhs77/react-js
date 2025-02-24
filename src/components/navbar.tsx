093876e7a264f670f48cfa6c8d95284e06b66fb8
Akhila K C <149565424+akhs77@users.noreply.github.com>
2025-02-24 21:39:07 +0530

    Initialize dependabot/npm_and_yarn/tailwindcss-4.0.7

---
+++ src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import DarkMode from './dark-mode';
+
+export default function Navbar() {
+  return (
+    <header className="fixed flex w-full justify-center py-10">
+      <div className="flex h-full w-full max-w-3xl items-center justify-center">
+        <div className="flex h-full w-full items-center justify-between">
+          <h1 className="text-base font-semibold">
+            React + TypeScript Starter
+          </h1>
+          <DarkMode />
+        </div>
+      </div>
+    </header>
+  );
+}
