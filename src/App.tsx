bc4347cfbc39de4fd47e0d39b98be1d96f1adc65
Akhila K C <149565424+akhs77@users.noreply.github.com>
2025-02-24 21:39:05 +0530

    Initialize dependabot/npm_and_yarn/react-router-dom-7.2.0

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import RootLayout from './components/root-layout';
+import Homepage from './pages/home';
+import NotFoundPage from './pages/not-found';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: '/',
+        element: <Homepage />,
+        index: true,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
